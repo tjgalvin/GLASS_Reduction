e9e90a815929567c2ea0f3e1cbb9b8f466aaf72b
Tim Galvin <gal16b@csiro.au>
2018-08-02 07:40:13 +0000

    Added script to batch run calibration scripts
    Initial script to run a collection of calibration scripts based on supplied days. If not days supplied, glob and run everything

---
+++ run_calibrations.py
@@ -0,0 +1,45 @@
+"""Script to batch run calibration scripts across all days. 
+
+At this point requires some manual tweaking to run on a specific script type
+
+Need to add option to handle this. 
+"""
+import os
+import sys
+import glob
+import pymir as mu
+from multiprocessing import Pool
+import subprocess as sp
+
+def run(s):
+    """Function to call python calibration script
+    
+    Arguments:
+        s {tuple} -- Element one is the calibration script
+                     the second is the working directory to execute in
+    """
+    p = sp.run(s[0].split(), cwd=s[1])
+
+# Should be OK for the moment. No data in 2020's yet
+if len(sys.argv) > 1:
+    # Assume user knows what they are doing
+    days = sys.argv[1:]
+else:
+    days = glob.glob('Data/201*')
+
+# Tuple of mirstr task to execute and the cwd to pass
+# to subprocess.run()
+jobs = []
+
+for day in days:
+    a =[("python3 reduce_5.py", day),
+        ("python3 reduce_5.py", day)
+        ]
+    jobs.extend(a)
+
+
+
+pool = Pool(10)
+result = pool.map(run, jobs)
+pool.close()
+pool.join()
