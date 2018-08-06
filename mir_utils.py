7925f217745d37d9ac1ee074e68ce0caf4998f24
tim.galvin <gal16b@csiro.au>
2018-08-06 12:01:53 +0800

    Auto detect objects/consistent file structure
    Added code to attempt to auto detect the objects in a particular observation. Less hassle about pre-configuring day specific scripts
    
    Moved code to generate and move files at the end of a calibration to the mir_utils.py script

--- mir_utils.py
+++ mir_utils.py
@@ -11,6 +11,7 @@ from pymir import mirstr as m
 from astropy.time import Time
 import astropy.units as u
 import os
+import sys
 import shutil as su
 
 # Get default logger set up in the reduction pipeline
@@ -102,48 +103,112 @@ def mosaic_pgflag(src):
 
 # -----------------------------------------------------------------------------
 
+# -----------------------------------------------------------------------------
+# Source book keeping operations
+# -----------------------------------------------------------------------------
+
+def derive_obs_sources(uvsplit, freq):
+    """Return the objects that were observed in an observation, including
+    the primary calibrator (almost certainly 1934-638), the secondary,
+    and the target sources.  
+    
+    Arguments:
+        uvsplit {mirstr} -- Executed mirstr with the uvsplit output
+        freq {str} -- The frequency of the observing data. Used as hook
+                      to get out a source from
+    """
+    primary_srcs = ['1934-638']
+    secondary_srcs = ['2245-328', '2312-319']
+    target_srcs = ['a','b','c','d','e','f']
+
+    primary = None
+    secondary = None
+    targets = []
+    for l in uvsplit.split():
+        if freq in l:
+            src = l.split('.')[0]
+            # 1934-628 should always be there
+            if src in primary_srcs:
+                primary = f"{src}.{freq}"
+            elif src in secondary_srcs:
+                secondary = f"{src}.{freq}"
+            elif src in target_srcs:
+                targets.append(f"{src}.{freq}")
+            else:
+                # Assume it is a mosaic source target
+                targets.append(f"{src}.{freq}")
+
+    if primary is None:
+        logger.log(logging.ERROR, 'Primary is empty. Exiting.')
+        sys.exit(0)
+    
+    if secondary is None:
+        logger.log(logging.ERROR, 'Secondary is empty. Exiting.')
+        sys.exit(0)
+
+    if len(targets) == 0:
+        logger.log(logging.ERROR, 'No target mosaic found. Exiting.')
+        sys.exit(0)
+
+    return primary, secondary, targets
+# -----------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------
 # Multiband move operation
 # -----------------------------------------------------------------------------
 
-def mv_uv(mode:str):
+def mv_uv(freq:str):
     """Helper function to move uv files into directories consistently among days
     
     Arguments:
-        mode {str} -- The mode of calibration (notsys/normal)
+        freq {str} -- The frequency of the pipeline
     """
-    if mode == 'notsys':
-        suffix = '_notsys'
-    else:
-        suffix = ''
-    
-    # Move the rpfit -> miriad files
-    if not os.path.exists(f'uv{suffix}'):
-        os.mkdir(f'uv{suffix}')
-    su.move(f'data1.uv', f'uv{suffix}')
-    su.move(f'data2.uv', f'uv{suffix}')
-    
-    # Move the ifsel1 source files
-    if not os.path.exists(f'f7700{suffix}'):
-        os.mkdir(f'f7700{suffix}')
-    for uv in glob('*.7700'):
-        su.move(uv, f'f7700{suffix}')
-
-    # Move the ifsel2 source files
-    if not os.path.exists(f'f9500{suffix}'):
-        os.mkdir(f'f9500{suffix}')
-    for uv in glob('*.9500'):
-        su.move(uv, f'f9500{suffix}')
-
+    if not os.path.exists('Plots'):
+        # Potential race conditions
+        try:
+            os.makedirs('Plots')
+        except:
+            pass
         
-        # Move the ifsel2 source files
-    if not os.path.exists(f'Plots{suffix}'):
-        os.mkdir(f'Plots{suffix}')
-    for uv in glob('*.png'):
-        su.move(uv, f'Plots{suffix}')
-    for txt in glob('*.txt'):
-        su.move(txt, f'Plots{suffix}')
+    for f in glob(f'*{freq}.png') + glob(f'*{freq}_log.txt'):
+        su.move(f, 'Plots')
+
+
+    if not os.path.exists(f'f{freq}'):
+        # Potential race conditions
+        try:
+            os.makedirs(f'f{freq}')
+        except:
+            pass
+        
+    for f in glob(f'*.{freq}'):
+        su.move(f, f'f{freq}')
+
+
+    if not os.path.exists('uv'):
+        # Potential race conditions
+        try:
+            os.makedirs('uv')
+        except:
+            pass
+
+    if freq == 5500:
+        su.move('data5.uv', 'uv')
+    elif freq == '9500':
+        su.move('data9.uv', 'uv')
 
 # -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    uvsplit_test = """uvsplit vis=data9.uv options=mosaic
+
+uvsplit: Revision 1.18, 2016/05/09 03:06:18 UTC
+
+Creating 1934-638.9500
+Creating 2245-328.9500
+Creating d.9500"""
+
+    print(derive_obs_sources(uvsplit_test, '9500'))
+    print(derive_obs_sources(uvsplit_test, '5500'))
 
