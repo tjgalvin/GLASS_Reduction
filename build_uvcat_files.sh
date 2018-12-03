1cd4b7ecc3156d02195c7305534487bb48c8d283
Tim Galvin <gal16b@csiro.au>
2018-12-03 03:53:14 +0000

    updated uv-file build script
    Need to add checks to make directories up. in time.

--- build_uvcat_files.sh
+++ build_uvcat_files.sh
@@ -1,2 +1,3 @@
-for i in $(cat pointings.txt | grep '_' | sort | uniq); do echo $i; echo uvcat in=201*/f9500/$i.9500 out=Pointings_f9500/$i.9500; done
+freq=$1
+for i in $(cat pointings.txt | grep '_' | sort | uniq); do echo  $i; uvcat vis=201*/f$freq/$i.$freq out=Pointings_f$freq/$i.$freq; done
 
