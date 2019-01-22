b9331c871b78dbbd1c3ad7a88850fe4474359454
Tim Galvin <gal16b@csiro.au>
2019-01-22 05:48:37 +0000

    Updated uvcat script

--- build_uvcat_files.sh
+++ build_uvcat_files.sh
@@ -1,3 +1,3 @@
 freq=$1
-for i in $(cat pointings.txt | grep '_' | sort | uniq); do echo  $i; uvcat vis=201*/f$freq/$i.$freq out=Pointings_f$freq/$i.$freq; done
+for i in $(cat "../pointings_f$freq.txt" | cut -d '/' -f4 | grep '_' | sort | uniq); do echo  $i; uvcat vis=201*/f$freq/$i out=Pointings_f$freq/$i ; done
 
