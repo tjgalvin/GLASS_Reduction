47fbc4921cc469bfac7f11c8832738d50638517f
Tim Galvin <gal16b@pearcey-login.cm.hpc>
2019-07-11 21:44:40 +1000

    modified time selections on some days
    also could not find a good solution for 2017-12-31 so removed it for the time being

--- build_uvcat_files.sh
+++ build_uvcat_files.sh
@@ -1,3 +1,3 @@
 freq=$1
-for i in $(cat "../pointings_f$freq.txt" | cut -d '/' -f4 | grep '_' | sort | uniq); do echo  $i; uvcat vis=201*/f$freq/$i out=Pointings_f$freq/$i ; done
+for i in $(cat "../pointings_f$freq.txt" | cut -d '/' -f4 | grep '_' | sort | uniq); do echo  $i; uvcat vis=../201*/f"$freq"_sources/$i out=$i ; done
 
